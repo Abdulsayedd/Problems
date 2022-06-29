7a7148633bef5b4ebc00d6ebbca73d2494922edb
Abdulsayed <mohammed321735@gmail.com>
2022-06-29 02:18:27 +0200

    Wednesday, June 29, 2022

--- 2022/June/Banknotes .cpp
+++ 2022/June/Banknotes .cpp
@@ -1,8 +1,6 @@
 #include <bits/stdc++.h>
 #define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
 using namespace std ;
-bool srt(const pair<string,string>&a,const pair<string,string>&b)
-{return (a.first<b.first);}
 int main ()
 {
     pre;
