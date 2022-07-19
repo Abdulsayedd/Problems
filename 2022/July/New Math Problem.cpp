3fa83eb80c1fac5cbf977d713391a85c4b6857a9
Abdulsayed <mohammed321735@gmail.com>
2022-07-19 22:26:46 +0200

    Tuesday, July 19, 2022

---
+++ 2022/July/New Math Problem.cpp
@@ -0,0 +1,10 @@
+//https://codeforces.com/group/UZxBcPr4qR/contest/247853/problem/A
+#include <bits/stdc++.h>
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std ;
+long long const MOD=1000000007;
+int main () {
+    pre;
+    string x;cin>>x;
+    cout<<0<<" "<<0;
+}
