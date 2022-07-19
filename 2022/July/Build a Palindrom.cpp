d9b7820825e6627fb0d9797b3aee9d078c086138
Abdulsayed <mohammed321735@gmail.com>
2022-07-20 01:28:49 +0200

    Wednesday, July 20, 2022

---
+++ 2022/July/Build a Palindrom.cpp
@@ -0,0 +1,9 @@
+// https://codeforces.com/group/UZxBcPr4qR/contest/249115/problem/H
+#include <bits/stdc++.h>
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std ;
+int main () {
+    pre;
+    int n,m;cin>>n;m=n;
+    for(int i = 0 ; i < n ; i++)cout<<'a';
+}
