14eb5549f4636aee2f70241b26586c38469c4b1c
Abdulsayed <mohammed321735@gmail.com>
2022-07-20 22:45:48 +0200

    Wednesday, July 20, 2022

---
+++ 2022/July/Word Capitalization.cpp
@@ -0,0 +1,10 @@
+// https://codeforces.com/problemset/problem/281/A
+#include <bits/stdc++.h>
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std ;
+int main () {
+    pre;
+    string x;cin>>x;
+    x[0]&='_';
+    cout<<x;
+}
