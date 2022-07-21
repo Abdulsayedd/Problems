9acf23b10cb950fc189cf03e3df98672fb9b38a6
Abdulsayed <mohammed321735@gmail.com>
2022-07-21 23:35:23 +0200

    Thursday, July 21, 2022

--- 2022/July/New Year and Old Property .cpp
+++ 2022/July/New Year and Old Property .cpp
@@ -1,28 +1,24 @@
-//============================
-// Name        : C. New Year and Old Property
-// Author      : Abdulsayed
-// Version     : 2022
-// Description : BitWise
-//============================
+// https://codeforces.com/group/4BawYTBUCN/contest/379570/problem/H
 #include <bits/stdc++.h>
-#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
-using namespace std ;
-int main () {
-    pre;
-    unsigned long long n,m,ans,c=0,calc;cin>>n>>m;
-    unsigned long long ns=(int)log2(n)+1,ms=(int)log2(m)+1;
-    for(unsigned long long i = ns ; i <=ms ; i++)
-    {
-        calc=(1llu<<(i))-1;// set 1 size
-        for(int j = 0 ; j < i-1 ; j++)
+using namespace std;
+long long fun(long long L, long long R)
+{
+    long long ans = 0;
+    long long LogR = log2(R) + 1;
+    for (long long i = 0; i < LogR; i++) {
+        long long cur = 0;
+        for (long long j = 0; j < i; j++)cur |= (1LL << j);
+        for (long long j = i + 1;j < LogR; j++)
         {
-            ans=calc-(1llu<<j);//1 10 100 1000 ....
-            /*bitset<64>b(calc);
-            bitset<64>a(ans);
-            cout<<b<<"\n"<<a;*/
-            if(ans>= n && ans<=m){c++;/*cout<<"*";*/}
-            /*cout<<"\n";*/
+            cur |= (1LL << j);
+            if (cur >= L && cur <= R)ans++;
         }
     }
-    cout<<c;
+    return ans;
+}
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    long long l,r;cin>>l>>r;
+    cout<<fun(l,r);
 }
