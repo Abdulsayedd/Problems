08bda5d1c26d4b31956caea7dab6813847a046bd
Abdulsayed <mohammed321735@gmail.com>
2022-07-24 20:53:03 +0200

    Sunday, July 24, 2022

---
+++ 2022/July/Remove a Progression .cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/contest/1194/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    int t;cin>>t;
+    while(t--)
+    {
+        long long n,x;cin>>n>>x;
+        cout<<x*2<<"\n";
+    }
+}
