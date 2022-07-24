981367fe89ebb09260f757c06dd41ef8d6510997
Abdulsayed <mohammed321735@gmail.com>
2022-07-24 20:53:13 +0200

    Sunday, July 24, 2022

---
+++ 2022/July/SpongeBob SquarePants .cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    int t;cin>>t;
+    while(t--)
+    {
+        long long n,m;cin>>n>>m;
+        cout<<(n==m?"YES":"NO")<<"\n";
+    }
+
+}
