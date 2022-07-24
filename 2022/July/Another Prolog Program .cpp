2fbdd9cd84889f8b0bde73f01485be73c87c1720
Abdulsayed <mohammed321735@gmail.com>
2022-07-24 20:52:14 +0200

    Sunday, July 24, 2022

---
+++ 2022/July/Another Prolog Program .cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    long long n;cin>>n;
+    cout<<n*4;
+}
