5a62aad61f01eacf7904566e69a1c8a4514fcc89
Abdulsayed <mohammed321735@gmail.com>
2022-07-25 16:57:02 +0200

    Monday, July 25, 2022

---
+++ 2022/July/Infinity Gauntlet.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    int t;cin>>t;
+    while(t--)
+    {
+    double h ;cin>>h;
+    cout<<fixed<<setprecision(1)<<h/2<<"\n";
+    }
+}
