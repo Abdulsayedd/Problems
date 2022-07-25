454f81663ea3563cfc8390efb411341c277ab25d
Abdulsayed <mohammed321735@gmail.com>
2022-07-25 16:56:43 +0200

    Monday, July 25, 2022

---
+++ 2022/July/BCD .cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+int main () {
+    pre;
+    long long n,m;cin>>n>>m;
+    cout<<ceil(1.0*n/m);
+}
