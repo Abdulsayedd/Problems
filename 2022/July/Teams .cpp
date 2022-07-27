fdd652c36ef4ba0c75ba00483a8c7bd9ebcf504b
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:55:25 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/Teams .cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/group/xkS6MyTkfj/contest/262216/problem/A
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ld long double
+int main () {
+    pre;
+    ll n;cin>>n;
+    cout<<n*3;
+}
