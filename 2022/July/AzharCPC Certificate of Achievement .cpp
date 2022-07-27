10d4a2b36cabfd6c31fff3d5e8da2064b60dcabf
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:53:53 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/AzharCPC Certificate of Achievement .cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/group/XGW5NIn8kq/contest/326459/problem/G
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main ()
+{
+    pre;
+    ll n,m;cin>>n>>m;
+    cout<<n*3+m;
+}
