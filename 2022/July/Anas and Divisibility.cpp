da6a0b24fbfe8b668164cff4aed3cd5e8daab25f
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:53:44 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/Anas and Divisibility.cpp
@@ -0,0 +1,11 @@
+//https://codeforces.com/group/XGW5NIn8kq/contest/326459/problem/C
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main ()
+{
+    pre;
+    string x;cin>>x;
+    cout<<"YES";
+}
