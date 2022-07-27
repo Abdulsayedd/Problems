9d70e60250910d52d6a42fcc56c401f8c9fab3d1
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:55:13 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/Odd and Even Count .cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define ll long long
+#define ull unsigned long long
+#define ld  long double
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+const double PI = 3.14159265358979323846;
+const ll MOD = 1000000007;
+int main (){
+        pre;
+        int t;cin>>t;
+        while(t--)
+        {
+            ll n;cin>>n;
+            if(n%2==0)cout<<n/2<<" "<<n/2<<"\n";
+            else cout<<n/2+1<<" "<<n/2<<"\n";
+        }
+
+}
