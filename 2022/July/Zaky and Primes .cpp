28cef00b34eac7a3ea68b4afe9fda48583c8f942
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:55:38 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/Zaky and Primes .cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/group/XGW5NIn8kq/contest/326459/problem/E
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main ()
+{
+    pre;
+    ll n;cin>>n;
+    if(n>=11)cout<<"YES";
+    else cout<<"NO";
+}
