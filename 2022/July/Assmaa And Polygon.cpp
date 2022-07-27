912e6a82ec8bde72b916504eefa6556183c33179
Abdulsayed <mohammed321735@gmail.com>
2022-07-27 21:53:49 +0200

    Wednesday, July 27, 2022

---
+++ 2022/July/Assmaa And Polygon.cpp
@@ -0,0 +1,12 @@
+//https://codeforces.com/group/XGW5NIn8kq/contest/326459/problem/F
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main ()
+{
+    pre;
+    ll n;cin>>n;
+    n-=2;n*=180;
+    cout<<n;
+}
