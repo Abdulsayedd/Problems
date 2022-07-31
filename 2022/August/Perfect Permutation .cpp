9b25b0b0ae4a7702ce79f3de531b82a117bf85db
Abdulsayed <mohammed321735@gmail.com>
2022-08-01 00:46:47 +0200

    Monday, August 1, 2022

---
+++ 2022/August/Perfect Permutation .cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main ()
+{
+        pre;
+        int t;cin>>t;
+        while(t--)
+        {
+            int n;cin>>n;
+            cout<<n<<" ";
+            for(int i = 1 ; i<n;i++)cout<<i<<" ";
+            cout<<"\n";
+        }
+
+}
