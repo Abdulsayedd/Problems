f8f83a9be24f87b7fcb4fe72fc668dd047381d64
Abdulsayed <mohammed321735@gmail.com>
2022-08-08 13:22:53 +0200

    Monday, August 8, 2022

---
+++ 2022/August/Logarithms.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+int main (){
+    pre;
+    double a,b;cin>>a>>b;
+    cout<<fixed<<setprecision(8)<<a*log2(b);
+}
