8ef918b035f0b959dba91ef6fac8e74e0aacc904
Abdulsayed <mohammed321735@gmail.com>
2022-08-20 21:39:50 +0200

    Saturday, August 20, 2022

---
+++ 2022/August/NIT orz!.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ull unsigned long long
+#define ld  long double
+priority_queue<ll,vector<ll>,greater<ll>>q;set<ll>s;multiset<ll>m_s;map<ll,ll>mp;
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+const double PI = 3.14159265358979323846;
+const ll MOD = 1000000007;
+int main () {
+    pre;
+    int t;cin>>t;
+    while(t--)
+    {
+        ll n,m,mx=INT_MIN;cin>>n>>m;
+        while(n--)
+        {
+            ll h;cin>>h;
+            s.insert((m|h));
+        }
+        cout<<*max_element(s.begin(),s.end())<<"\n";
+        s.clear();
+    }
+}
