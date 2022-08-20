279e07c97eb8c2d35ab70c10162844c361d340c4
Abdulsayed <mohammed321735@gmail.com>
2022-08-20 21:39:25 +0200

    Saturday, August 20, 2022

---
+++ 2022/August/Extra Long Factorials.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ull unsigned long long
+#define ld  long double
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+const double PI = 3.14159265358979323846;
+const ll MOD = 1000000007;
+const ll MAX = 500;
+int multiply(int x, int res[], int res_size)
+{
+    int carry = 0;
+    for (int i=0; i<res_size; i++)
+    {
+        int prod = res[i] * x + carry;
+        res[i] = prod % 10;
+        carry = prod/10;
+    }
+    while (carry)
+    {
+        res[res_size] = carry%10;
+        carry = carry/10;
+        res_size++;
+    }
+    return res_size;
+}
+void factorial(int n)
+{
+    int res[MAX];
+    res[0] = 1;
+    int res_size = 1;
+    for (int x=2; x<=n; x++)
+        res_size = multiply(x, res, res_size);
+    for (int i=res_size-1; i>=0; i--)
+        cout << res[i];
+}
+int main (){
+    pre;
+    int n;cin>>n;
+    factorial(n);
+}
