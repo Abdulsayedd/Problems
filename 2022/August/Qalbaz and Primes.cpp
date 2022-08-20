ac869fcd3363598c124c7e78d135cb9271c4a6c5
Abdulsayed <mohammed321735@gmail.com>
2022-08-20 21:39:57 +0200

    Saturday, August 20, 2022

---
+++ 2022/August/Qalbaz and Primes.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define ll long long
+#define ull unsigned long long
+#define ld  long double
+priority_queue<ll,vector<ll>,greater<ll>>q;set<ll>s;multiset<ll>m_s;map<ll,ll>mp;
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+const double PI = 3.14159265358979323846;
+const ll MOD = 1000000007;
+const ll MAX = 100007;
+vector<ll>v;
+ll freq[MAX];
+void SieveOfEratosthenes(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+    for (int p = 2; p * p <= n; p++) {
+        if (prime[p] == true) {
+            for (int i = p * p; i <= n; i += p)
+                prime[i] = false;
+        }
+    }
+
+    for (int p = 2; p <= n; p++)
+        if (prime[p])
+            v.push_back(p);
+}
+int main (){
+    pre;
+    int n,q;cin>>n>>q;
+    SieveOfEratosthenes(n+1);
+    freq[0]=0;
+    for(int i = 1 ; i<v.size(); i++)
+    {
+        freq[i]=freq[i-1]+v[i-1];
+    }
+    while(q--)
+    {
+        int l,r;cin>>l>>r;
+        cout<<freq[r]-freq[l-1]<<"\n";
+    }
+}
