21b537580edf13bf6ce3d3d1ed3d787c9b68a80b
Abdulsayed <mohammed321735@gmail.com>
2022-08-21 19:46:48 +0200

    Sunday, August 21, 2022

---
+++ 2022/August/Mathematical Field of Experiments .cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define ull unsigned long long
+#define ld  long double
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+const double PI = 3.14159265358979323846;
+const int MOD = 1000000007;
+bool comp(pair<int,pair<int,int>>p,pair<int,pair<int,int>>p1)
+{
+    return p.first>p1.first;
+}
+int arr[2000007];
+signed main (){
+    pre;
+    int n;
+    cin>>n;
+    cout<<0<<" ";
+    for(int i = 1 ; i <=n/2;i++)
+    {
+        arr[i*i%n]=i;
+    }
+    for(int i =1 ; i< n ; i++)
+    {
+        cout<<(arr[i]?arr[i]:-1)<<" ";
+    }
+}
