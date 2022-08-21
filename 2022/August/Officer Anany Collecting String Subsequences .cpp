2b1a15546e2b856609c21b9bb937c8b9be9054e2
Abdulsayed <mohammed321735@gmail.com>
2022-08-21 19:46:56 +0200

    Sunday, August 21, 2022

---
+++ 2022/August/Officer Anany Collecting String Subsequences .cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define ull unsigned long long
+#define ld  long double
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+#define what(x) cerr << #x << " is " << x << "\n";
+const double PI = 3.14159265358979323846;
+const int MOD = 1000000007;
+signed main (){
+    pre;
+    freopen("collectingofficer.in","r",stdin);
+    int t;cin>>t;
+    while(t--)
+    {
+        int n;cin>>n;
+        string x;cin>>x;
+        int l;
+        int mn=INT_MAX;
+        for(int i = 0 ; i < x.size() ; i++)
+        {
+            if(x[i]=='A')
+            {
+                l=i;
+                char chh='A';
+                int cntch=26;
+                for(int j = i ; j <x.size() ; j++)
+                {
+                    if(x[j]==chh)
+                    {
+                        chh++;
+                        cntch--;
+                    }
+                    if(!cntch)
+                    {
+                        mn=min(mn,j-l);
+                        //what(mn);
+                    }
+                }
+            }
+        }
+        cout<<mn+1<<"\n";
+    }
+}
