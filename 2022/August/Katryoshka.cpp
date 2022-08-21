60bbe1e8c5d39db215cd7b7777ab0cfc539333f5
Abdulsayed <mohammed321735@gmail.com>
2022-08-21 19:46:36 +0200

    Sunday, August 21, 2022

---
+++ 2022/August/Katryoshka.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+using namespace std;
+typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> ind_set;
+#define pre ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define int long long
+#define ull unsigned long long
+#define ld  long double
+#define fx(x) fixed<<setprecision(x)
+#define split(x,ch,v) stringstream ss(x);string s; while(getline(ss,s,ch))v.push_back(s);
+#define fx(x) fixed<<setprecision(x)
+#define pb(x) emplace_back(x)
+#define lower(x) for(int i=0;i<x.size();i++)x[i]|=' ';
+#define upper(x) for(int i=0;i<x.size();i++)x[i]&='_';
+#define what(x) cerr << #x << " is " << x << endl;
+const double PI = 3.14159265358979323846;
+const int MOD = 1000000007;
+signed main (){
+    pre;
+    freopen("katryoshka.in","r",stdin);
+    int t;cin>>t;
+    for(int j = 1 ; j<=t ; j++)
+    {
+        int a,b,c;
+        int sum=0;
+        cin>>a>>b>>c;
+        int h=min({a,b,c});
+        a-=h;
+        b-=h;
+        c-=h;
+        sum+=h;
+        if(a>=2 and c and !b)
+        {
+            a/=2;
+            int hh=min(a,c);
+            sum+=hh;
+        }
+        cout<<"Case "<<j<<": "<<sum<<"\n";
+    }
+}
